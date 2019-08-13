9ead5d8b8af60681cc66ec60a922e8cb7f9a3e51
saurusss <40891607+saurusss@users.noreply.github.com>
2019-08-14 01:01:27 +0900

    work at home bootstrap, user:references

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.5.5'
 
 gem 'devise'
+gem 'bootstrap-sass'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :posts
   get '/' => 'home#index'
   get '/about' => 'home#about'
+  post '/posts/:post_id' =>"comments#create"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
