f17df4da7bd2c26757826dcddf2fb5246754241b
saurusss <40891607+saurusss@users.noreply.github.com>
2019-08-24 19:41:06 +0900

    work at home

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,4 +8,20 @@ class CommentsController < ApplicationController
         
         redirect_to @post
     end
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+
+        redirect_to @post       
+    end
+    def update
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.comment = params[:comment]
+        @comment.user_id = params[:user_id]
+        @comment.save      
+
+        redirect_to @post       
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/' => 'home#index'
   get '/about' => 'home#about'
   post '/posts/:post_id' =>"comments#create"
+  delete '/posts/:post_id/comments/:id' =>"comments#destroy"
+  post '/posts/:post_id/comments/:id' => 'comments#update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
